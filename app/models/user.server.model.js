a020d01324edcf4ed4aaf6920b7c844d782ceaed
driden <driden_87@hotmail.com>
2017-12-01 15:34:23 -0300

    porquerias

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,3 +1,5 @@
+import { Buffer } from 'buffer';
+
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
@@ -25,6 +27,16 @@ const UserSchema = new Schema({
             (password) => password.length >= 6,'Password should be longer than 5 characters'
         ]
     },
+    salt:{
+        type: String,
+        
+    },
+    provider: {
+        type: String,
+        required: 'Provider is required'
+    },
+    providerId: String,
+    providerData: {},
     created: {
         type: Date,
         default: Date.now
@@ -49,6 +61,19 @@ UserSchema.virtual('fullName')
         firstName = splitName[0] || ''
         lastName = splitName[1] || ''
     })
+
+UserSchema.pre('save', (next) => {
+    if (this.password){
+        this.salt = new Buffer(crypto.randomBytes(16).toString('base64'),'base64')
+        this.password = this.hashPassword(this.password)
+    }
+    next()
+})
+
+UserSchema.methods.hashPassword = (password) => 
+    crypto.pbkdf2sync(password, this.salt, 10000,64).toString('base64')
+
+UserSchema.methods.authenticate = (password) => this.password === this.hashPassword(this.password)
 
 UserSchema.statics.findOneByUsername = (username, callback) => this.findOne( {username: new RegExp(username, 'i')}, callback)
 UserSchema.methods.authenticate = (password) => this.password === password
