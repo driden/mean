17ec2ef8d1f17010e69a7e8a6a2377732a161d1e
driden <driden_87@hotmail.com>
2017-12-08 17:55:51 -0300

    twitter inicio

--- config/env/development.js
+++ config/env/development.js
@@ -6,5 +6,10 @@ module.exports = {
         clientID: '1754474704585696',
         clientSecret: 'fc39c3776f71ebfd6d8a0c9e083e97c6',
         callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+    },
+    twitter: {
+        clientID: 'WGHqK4Qu4xAyeyVa68isqlbj9',
+        clientSecret: 'zloltLmIAq9rPzdQxUU3DO0VByqgzWXXg33h16bEjwFHEBfiHy',
+        callbackURL: 'http://localhost:3000/oauth/twitter/callback'
     }
 }
