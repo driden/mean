b33ea71098aac2b199935c12afd6bfbef8b97603
driden <driden_87@hotmail.com>
2018-01-02 17:40:36 -0300

    templates de autenticacion

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -22,63 +22,48 @@ function getErrorMessage(err) {
     return message
 }
 
-exports.signin = (req,res,next) => {
-    passport.authenticate('local', (err,user,info) => {
-        if (err || user){
+exports.signin = (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err || !user) {
             res.status(400).send(info)
         } else {
             user.password = undefined
             user.salt = undefined
 
             req.login(user, (err) => {
-                if (err){
+                if (err) {
                     res.status(400).send(err)
                 } else {
                     res.json(user)
                 }
             })
         }
-    }) (req, res, next)
-}
-
-exports.renderSignin = (req, res, next) => {
-    if (!req.user)
-        res.render('signin', {
-            title: 'Sign-in Form',
-            messages: req.flash('error') || req.flash('info')
-        })
-    else
-        return res.redirect('/')
-}
-
-exports.renderSignup = (req, res, next) => {
-    if (!req.user)
-        res.render('signup', {
-            title: 'Sign-up Form',
-            messages: req.flash('error')
-        })
-    else
-        return res.redirect('/')
+    })(req, res, next)
 }
 
 exports.signup = (req, res, next) => {
-    if (!req.user) {
-        const user = new User(req.body)
-        user.provider = 'local'
 
-        user.save((err) => {
-            if (err) {
-                const message = getErrorMessage(err)
+    const user = new User(req.body)
+    user.provider = 'local'
 
-                req.flash('error', message)
-                return res.redirect('/signup')
-            }
-            req.login(user, (err) => {
-                if (err) return next(err)
-                return res.redirect('/')
+    user.save((err) => {
+        if (err) {
+            return res.status(400).send({
+                message: getErrorMessage(err)
             })
-        })
-    } else return res.redirect('/')
+        } else {
+            user.password = undefined
+            user.salt = undefined
+
+            req.login(user, (err) => {
+                if (err) {
+                    res.status(400).send(err)
+                } else {
+                    res.json(user)
+                }
+            })
+        }        
+    })
 }
 
 exports.signout = (req, res) => {
@@ -86,32 +71,32 @@ exports.signout = (req, res) => {
     res.redirect('/')
 }
 
-exports.saveOAuthUserProfile = function(req, profile, done){
+exports.saveOAuthUserProfile = function (req, profile, done) {
     User.findOne({
-        provider : profile.provider,
+        provider: profile.provider,
         providerId: profile.providerId
-    }, (err, user) =>{
-        
-        if(err) return done(err)
+    }, (err, user) => {
+
+        if (err) return done(err)
 
         if (!user) {
-            const possibleUsername = 
+            const possibleUsername =
                 profile.username || ((profile.email) ? profile.email.split('@')[0] : '')
 
-            User.findUniqueUsername(possibleUsername,null, (availableUsername) => {
+            User.findUniqueUsername(possibleUsername, null, (availableUsername) => {
                 const newUser = new User(profile)
                 newUser.username = availableUsername
                 newUser.save((err) => {
                     if (err) {
                         const message = _this.getErrorMessage(err)
-                        req.flash('error',message)
+                        req.flash('error', message)
                         return res.redirect('/signup')
                     }
                     return done(err, newUser)
                 })
             })
         } else
-           return done(err,user)
-           
+            return done(err, user)
+
     })
 }
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -2,49 +2,44 @@ const users = require('../controllers/users.server.controller')
 const passport = require('passport')
 
 module.exports = (app) => {
-    app.route('/signup')
-        .get(users.renderSignup)
-        .post(users.signup)
-    
-    app.route('/signin')
-        .get(users.renderSignin)
-        .post(passport.authenticate('local',{
-            successRedirect: '/',
+    app.route('/api/auth/signup').post(users.signup)
+    app.route('/api/auth/signin').post(users.signin)
+    app.route('/api/auth/signout').post(users.signout)
+
+    app.get('/api/oauth/facebook', passport.authenticate('facebook', {
+        failureRedirect: '/signin'
+    }))
+
+    app.get('api/oauth/facebook/callback',
+        passport.authenticate('facebook', {
             failureRedirect: '/signin',
-            failureFlash: true
+            successRedirect: '/',
+            scope: ['email', "public_profile"]
         }))
 
-    app.get('/signout', users.signout)
-
-    app.get('/oauth/facebook', passport.authenticate('facebook',{
-        failureRedirect: '/signin'
+    app.get('api/oauth/twitter',
+        passport.authenticate('twitter', {
+            failureRedirect: '/signin'
     }))
 
-    app.get('/oauth/facebook/callback', passport.authenticate('facebook',{
-        failureRedirect: '/signin',
-        successRedirect: '/',
-        scope: ['email',"public_profile"]
+    app.get('api/oauth/twitter/callback',
+        passport.authenticate('twitter', {
+            failureRedirect: '/signin',
+            successRedirect: '/'
     }))
 
-    app.get('/oauth/twitter', passport.authenticate('twitter',{
-        failureRedirect: '/signin'
+    app.get('api/oauth/google',
+        passport.authenticate('google', {
+            failureRedirect: '/signin',
+            scope: [
+                'https://www.googleapis.com/auth/userinfo.profile',
+                'https://www.googleapis.com/auth/userinfo.email'
+            ]   
     }))
 
-    app.get('/oauth/twitter/callback', passport.authenticate('twitter',{
-        failureRedirect: '/signin',
-        successRedirect: '/'
-    }))
-
-    app.get('/oauth/google', passport.authenticate('google',{
-        failureRedirect: '/signin',
-        scope: [
-            'https://www.googleapis.com/auth/userinfo.profile',
-            'https://www.googleapis.com/auth/userinfo.email'
-        ]
-    }))
-    
-    app.get('/oauth/google/callback', passport.authenticate('google',{
-        failureRedirect: '/signin',
-        successRedirect: '/'
+    app.get('api/oauth/google/callback',
+        passport.authenticate('google', {
+            failureRedirect: '/signin',
+            successRedirect: '/'
     }))
 }
